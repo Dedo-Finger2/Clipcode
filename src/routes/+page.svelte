50c7be9a81f7ff26058909539d17dc3d21e87170
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-12 06:40:50 -0300

    feat: add data-binding to all inputs in CodeSnippetCard component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,51 @@
 <script lang="ts">
 	import CodeSnippetCard from "../CodeSnippetCard.svelte";
+
+	// Variável que já está sendo tratada como uma variável de estado
+	let formData: CodeSnippetInput = {
+		title: "",
+		code: "",
+		language: "html"
+	}
 </script>
 
 <div class="flex justify-center p-5">
 	<div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
 		<h3 class="text-center py-6 text-3xl">Create a Code Snippet</h3>
 		<div class="card p-4 w-full text-token space-y-4 rounded-2xl shadow-md">
+
 			<label class="label">
 				<span>Snippet Tittle</span>
-				<input class="input rounded-lg" type="text" placeholder="Enter the tittle her...">
+				<input
+					class="input rounded-lg"
+					type="text"
+					placeholder="Enter the tittle her..."
+					bind:value={formData.title}
+				>
 			</label>
+
 			<label class="label">
 				<span>Programming Language</span>
-				<select class="select rounded-lg">
+				<select
+					class="select rounded-lg"
+					bind:value={formData.language}
+				>
 					<option value="html">HTML</option>
 					<option value="php">PHP</option>
 					<option value="typescript">TypeScript</option>
 				</select>
 			</label>
+
 			<label class="label">
 				<span>Code Snippet</span>
-				<textarea class="textarea rounded-lg" rows="4" placeholder="Paste your code here..."></textarea>
+				<textarea
+					class="textarea rounded-lg"
+					rows="4"
+					placeholder="Paste your code here..."
+					bind:value={formData.code}
+				></textarea>
 			</label>
+
 		</div>
 
 		<div>
