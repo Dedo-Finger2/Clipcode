b475782febe0dcfb44b451cfc512c62af9a19887
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-12 06:27:46 -0300

    feat: add home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,27 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<div class="flex justify-center p-5">
+	<div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
+		<h3 class="text-center py-6 text-3xl">Create a Code Snippet</h3>
+		<div class="card p-4 w-full text-token space-y-4 rounded-2xl shadow-md">
+			<label class="label">
+				<span>Snippet Tittle</span>
+				<input class="input rounded-lg" type="text" placeholder="Enter the tittle her...">
+			</label>
+			<label class="label">
+				<span>Programming Language</span>
+				<select class="select rounded-lg">
+					<option value="html">HTML</option>
+					<option value="php">PHP</option>
+					<option value="typescript">TypeScript</option>
+				</select>
+			</label>
+			<label class="label">
+				<span>Code Snippet</span>
+				<textarea class="textarea rounded-lg" rows="4" placeholder="Paste your code here..."></textarea>
+			</label>
+		</div>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+		<div>
+			<h2 class="text-2xl">My Code Snippets</h2>
+		</div>
 	</div>
 </div>
