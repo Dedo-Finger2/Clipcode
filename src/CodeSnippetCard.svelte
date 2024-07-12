21ea0ed3a6fc9bc6c58f60e5e47f74e976431353
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-12 06:33:43 -0300

    feat: add rounded corners to the component

--- src/CodeSnippetCard.svelte
+++ src/CodeSnippetCard.svelte
@@ -3,14 +3,14 @@
   import { CodeBlock } from "@skeletonlabs/skeleton";
 </script>
 
-<div class="card">
+<div class="card rounded-2xl">
   <header class="card-header">
     Title
     <div class="float-right">
-      <button type="button" class="btn btn-sm variant-filled-secondary">
+      <button type="button" class="btn btn-sm variant-filled-secondary rounded-md">
         Favorite
       </button>
-      <button type="button" class="btn btn-sm variant-filled-error">
+      <button type="button" class="btn btn-sm variant-filled-error rounded-md">
         X
       </button>
     </div>
